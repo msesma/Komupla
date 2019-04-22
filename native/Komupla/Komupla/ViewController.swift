fa715bda8250fe91a69edcd0cb29373e6ddf022d
Miguel Sesma <miguel@sesma.eu>
2019-04-22 12:17:52 +0100

    Add empty iOS project

---
+++ native/Komupla/Komupla/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Komupla
+//
+//  Created by Miguel Sesma on 22/04/2019.
+//  Copyright © 2019 Sesma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
